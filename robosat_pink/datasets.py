92a79fe79500f48e60a9818ae0b6ddfe65e4e130
Tony Cannistra <tony.cannistra@gmail.com>
2019-04-24 14:57:25 -0700

    datatype

--- robosat_pink/datasets.py
+++ robosat_pink/datasets.py
@@ -246,7 +246,7 @@ class MultiSlippyMapTilesConcatenation(torch.utils.data.Dataset):
             data = augmented['image']
             mask = augmented['mask']
 
-        return(torch.DoubleTensor(data), torch.from_numpy(mask), match.name)
+        return(torch.FloatTensor(data), mask, match.name)
 
 
 
